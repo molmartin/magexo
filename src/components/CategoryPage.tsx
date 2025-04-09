21ec3047586561da1aaf88cb3facc33b3f474d0c
zmardoc <mol1@seznam.com>
2025-04-10 01:42:27 +0200

    layout

--- src/components/CategoryPage.tsx
+++ src/components/CategoryPage.tsx
@@ -1,44 +1,32 @@
-import type { FC } from 'react'
+import { useMemo, type FC } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import useProductQuery, { type Product } from '../composables/useProductQuery'
-
-const ProductCard: FC<{ product: Product }> = ({ product }) => {
-  return (
-    <li>
-      <h4>{product.title}</h4>
-      <ul>
-        {product.variants.edges.map(({ node: variant }) => (
-          <li key={variant.id}>
-            {variant.title} - {variant.priceV2.amount}{' '}
-            {variant.priceV2.currencyCode}
-          </li>
-        ))}
-      </ul>
-    </li>
-  )
-}
+import useProductQuery from '../composables/useProductQuery'
+import ProductCard from './ProductCard'
 
 const CategoryPage: FC = () => {
   const { categoryId } = useParams<{ categoryId: string }>()
 
   const { data, loading, error } = useProductQuery(categoryId) // TODO: handle error when categoryId is undefined
+  // TODO proverit, mozna  zbytecne
+  const products = useMemo(
+    () => data?.collection.products.edges,
+    [data?.collection.products.edges],
+  )
 
   if (loading) return <div>Loading...</div>
   if (error || !data) return <div>Error: {error?.message}</div>
 
-  const { collection } = data
-
   return (
-    <div>
+    <section>
       <Link to={`/`}>Back</Link>
-      <h2>{collection.title}</h2>
+      <h2>{data?.collection.title}</h2>
       <h3>Products</h3>
-      <ul>
-        {collection.products.edges.map(({ node: product }) => (
-          <ProductCard product={product} />
+      <div>
+        {products?.map(({ node: product }) => (
+          <ProductCard key={product.id} product={product} />
         ))}
-      </ul>
-    </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,19 @@
+import { FC, memo } from 'react'
+import { Product } from '../composables/useProductQuery'
+
+const ProductCard: FC<{ product: Product }> = memo(({ product }) => {
+  return (
+    <div>
+      <h4>{product.title}</h4>
+      <ul>
+        {product.variants.edges.map(({ node: variant }) => (
+          <li key={variant.id}>
+            {variant.title} - {variant.priceV2.amount}{' '}
+            {variant.priceV2.currencyCode}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+})
+export default ProductCard
